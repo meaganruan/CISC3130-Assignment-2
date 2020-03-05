2cf58f4f4496958419d7172ff4cb7be967fc6718
meaganruan <54692293+meaganruan@users.noreply.github.com>
2020-03-04 21:42:04 -0500

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class assignment2 {
 	public static void main(String[] args) throws IOException {
 		//store each file path into an array list
-		String f1 = "\"C://Users//meaga//Documents//CODE//CISC3130//CSVFiles//A1Charts.csv\"";
-		String f2 = "\"C://Users//meaga//Documents//CODE//CISC3130//CSVFiles//UKCHARTS.csv\"";
-		String f3 = "\"C://Users//meaga//Documents//CODE//CISC3130//CSVFiles//GLOBALCHARTS.csv\"";
+		File f1 = new File("C:\\Users\\meaga\\Documents\\CODE\\CISC3130\\CSVFiles\\A1Charts.csv");
+		File f2 = new File("C:\\Users\\meaga\\Documents\\CODE\\CISC3130\\CSVFiles\\UKCHARTS.csv");
+		File f3 = new File("C:\\Users\\meaga\\Documents\\CODE\\CISC3130\\CSVFiles\\GLOBALCHARTS.csv");
 
 		// to store multiple input files
-		ArrayList<String> files = new ArrayList<String>();
+		ArrayList<File> files = new ArrayList<File>();
 		files.add(f1);
 		files.add(f2);
 		files.add(f3);
@@ -18,8 +18,8 @@ public class assignment2 {
 		Queue<String> songList = new LinkedList<String>();
 
 		//try-catch to take songs out from files and putting them into the list
-		System.out.println(files);
-//		try {
+//		System.out.println(files);
+		try {
 			for (int i = 0; i < files.size(); i++) {
 				if (files.get(i) != null) {
 				BufferedReader reader = new BufferedReader(new FileReader(files.get(i)));
@@ -27,23 +27,21 @@ public class assignment2 {
 						String[] data = row.split(",");
 						String song = data[1];
 						songList.add(song);
-						reader.close();
+						//remove column name
+						songList.remove("Track Name");
 					}
+
+					reader.close();
 				}
 			}
-//		}
+		}
 //
-//		catch (Exception e) {
-//			System.out.println("Unreadable");
-//		}
+		catch (Exception e) {
+			System.out.println("Unreadable");
+		}
 
-		// read files in order and put items (songs) in a queue.
-//		Queue<String> orderedQueue = new LinkedList<String>();
-//		for (int j = 0; j < songList.size(); j++) {
-//		}
 		System.out.println(songList);
 
-		// merge all the queues together into a linked list.
 		// use stack to count the most recent inserted item.
 	}
 }
